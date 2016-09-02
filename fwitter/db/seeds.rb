03a0af052111a2b64877b0196e1b38312d6c2a97
JJ Seymour <jjseymour@gmail.com>
2016-09-01 23:49:42 -0400

    db seed

--- fwitter/db/seeds.rb
+++ fwitter/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@user = User.create(username: Faker::GameOfThrones.house, password: "I always pay my debt")
+
+10.times do
+  Tweet.create(content: Faker::ChuckNorris.fact, user_id: @user.id)
+end
